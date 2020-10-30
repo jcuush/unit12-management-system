8c2000e9fe872f063da26ed64f91dea6c466704e
jcuush <jrcushing@sbcglobal.net>
2020-10-29 20:17:04 -0500

    made functions to add new employee, department and role

--- server.js
+++ server.js
@@ -81,19 +81,46 @@ function viewCompany(){
 
 }
 
-// function updateCompany() {
+function updateCompany() {
 
-// }
+}
 
-// function addCompany() {
+function addCompany() {
+    inquirer
+    .prompt({
+        type: "list",
+        message:"What would you like to add",
+        name: "add",
+        choices: [
+            "Department",
+            "Role",
+            "Employee"
+        ]
 
-// }
+    }).then(function(answer) {
+        switch (answer.add) {
+            case "Department":
+                addDepartment();
+                break;
+                
+            case "Role":
+                addRole();
+                break;
+    
+            case "Employee":
+                addEmployee();
+                break;
+
+        }
+    })
+};
+
 
 function viewDepartments() {
     let query = "SELECT * FROM department";
     connection.query(query, function(err,res) {
-        console.log("success");
-        // console.table(res);
+        console.log("\n\n");
+        console.table(res);
     })
 
 }
@@ -101,31 +128,134 @@ function viewDepartments() {
 function viewRoles() {
     let query = "SELECT * FROM role";
     connection.query(query, function(err,res) {
-        console.log("success");
-        // console.table(res);
-        
+        console.log("\n\n");
+        console.table(res);  
     })
 }
 
 function viewEmployees() {
     let query = "SELECT * FROM employee";
     connection.query(query, function(err,res) {
-        console.log("success");
-        // console.table(res);
+        console.log("\n\n");
+        console.table(res);   
     })
 }
 
 function viewEntireCompany () {
-    let query = `SELECT employee.id, first_name, last_name, title, salary, name, manager_id 
-    FROM employee 
-    JOIN role 
-    ON role_id = role.id 
-    JOIN department 
-    ON department_id = department.id`;
-    connection.query(query, function(err,res) {
+    let query = `SELECT employee.id, first_name, last_name, title, salary, name, manager_id FROM employee JOIN role on role_id = role.id JOIN department ON department_id = department.id`
+    connection.query(query, function(err, result) {
         if (err) throw err;
         console.log("\n\n");
-        console.log("success");
-        // console.table(res);
+        console.table(result);
     })
+}
+
+function addDepartment () {
+    inquirer.prompt({
+        type:"input",
+        message:"What is the new department",
+        name: "newDepartment"
+    }).then(function(res){
+        let query = `INSERT INTO department (name) VALUES (?)`
+        connection.query(query,[res.newDepartment], function (err,res) {
+            console.log(err, res);
+        })
+    });
+    
+};
+
+function addRole () {
+    let query = `SELECT * FROM department`
+    connection.query(query, function(err, result) {
+    // console.log(err, result);
+
+
+        let departmentArray = [];
+
+        for (let i = 0; i < result.length; i++) {
+            departmentArray.push(result[i].name);
+        }
+
+    inquirer.prompt([
+        {
+        type:"input",
+        message:"What is the new role",
+        name: "newRole",
+        },
+
+        {
+        type:"input",
+        message:"What is the new salary",
+        name: "newSalary",
+        },
+
+        {
+        type:"list",
+        message:"What is the department of the role",
+        choices: departmentArray,
+        name: "roleDepartment",
+        }
+
+    ]).then(function(ans){
+        let departmentID;
+        for(let i = 0; i < result.length; i++) {
+            if(result[i].name === ans.roleDepartment) {
+                departmentID = result[i].id;
+            }
+        }
+        let query = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`
+        connection.query(query,[ans.newRole, ans.newSalary, departmentID], function (err,res) {
+            if (err) throw err;
+            
+        })
+    });
+});
+    
+}
+
+function addEmployee () {
+    let query = `SELECT * FROM role`
+    connection.query(query, function(err, result) {
+    // console.log(err, result);
+
+
+        let roleArray = [];
+
+        for (let i = 0; i < result.length; i++) {
+            roleArray.push(result[i].title);
+        }
+    inquirer.prompt([
+        {
+        type:"input",
+        message:"What is the new employee's first name?",
+        name: "newFirst",
+        },
+
+        {
+        type:"input",
+        message:"What is the new employee's last name?",
+        name: "newLast",
+        },
+
+        {
+        type:"list",
+        message:"What is the role of the new employee",
+        choices: roleArray,
+        name: "employeeRole",
+        }
+
+    ]).then(function(ans){
+        let roleID;
+        for(let i = 0; i < result.length; i++) {
+            if(result[i].name === ans.employeeRole) {
+                roleID = result[i].id;
+            }
+        }
+        let query = `INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)`
+        connection.query(query,[ans.newFirst, ans.newLast, roleID], function (err,res) {
+            if (err) throw err;
+        });
+    });
+    });
+
 }
