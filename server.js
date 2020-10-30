bd573b583a949d538ddfb59b02af2dc1c9e12a00
jcuush <jrcushing@sbcglobal.net>
2020-10-29 19:25:32 -0500

    made all view functions

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connection.connect(function(err) {
 function init() {
     inquirer.prompt({
         type:"list",
-        message:"What would you like to do to the company?",
+        message:"What would you like to do with company?",
         name: "action",
         choices: [
             "View",
@@ -49,16 +49,83 @@ function init() {
 }
 
 function viewCompany(){
+    inquirer.prompt({
+        type:"list",
+        message: "What would you like to view?",
+        name: "view",
+        choices: [
+            "Departments",
+            "Roles",
+            "Employees",
+            "Entire Company"
+        ]
+    }).then(function(answer) {
+        switch (answer.view) {
+            case "Departments":
+                viewDepartments();
+                break;
+                
+            case "Roles":
+                viewRoles();
+                break;
+    
+            case "Employees":
+                viewEmployees();
+                break;
+            
+            case "Entire Company":
+                viewEntireCompany();
+                break;
+        }
+    })
 
 }
 
-function updateCompany() {
+// function updateCompany() {
+
+// }
+
+// function addCompany() {
+
+// }
+
+function viewDepartments() {
+    let query = "SELECT * FROM department";
+    connection.query(query, function(err,res) {
+        console.log("success");
+        // console.table(res);
+    })
 
 }
 
-function addCompany() {
-
+function viewRoles() {
+    let query = "SELECT * FROM role";
+    connection.query(query, function(err,res) {
+        console.log("success");
+        // console.table(res);
+        
+    })
 }
 
+function viewEmployees() {
+    let query = "SELECT * FROM employee";
+    connection.query(query, function(err,res) {
+        console.log("success");
+        // console.table(res);
+    })
+}
 
-
+function viewEntireCompany () {
+    let query = `SELECT employee.id, first_name, last_name, title, salary, name, manager_id 
+    FROM employee 
+    JOIN role 
+    ON role_id = role.id 
+    JOIN department 
+    ON department_id = department.id`;
+    connection.query(query, function(err,res) {
+        if (err) throw err;
+        console.log("\n\n");
+        console.log("success");
+        // console.table(res);
+    })
+}
